0cc664101c0b5ef29feff0269be715d7ef81fe3d
root <root@debian.server.163.org>
2013-12-21 18:47:53 +0800

    netlink user space code not bind

--- network/netlink/netlink_kernel/user/netlink.c
+++ network/netlink/netlink_kernel/user/netlink.c
@@ -1,26 +1,36 @@
 #include <sys/socket.h>
 #include <linux/netlink.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
 #define NETLINK_USER 31
 
 #define MAX_PAYLOAD 1024 /* maximum payload size*/
-struct sockaddr_nl src_addr, dest_addr;
+//struct sockaddr_nl src_addr, dest_addr;
+struct sockaddr_nl dest_addr;
 struct nlmsghdr *nlh = NULL;
 struct iovec iov;
 int sock_fd;
 struct msghdr msg;
 
-void main()
+int main()
 {
 		sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
 		if (sock_fd < 0)
 				return -1;
+//http://inai.de/documents/Netlink_Protocol.pdf
+//
+//If  bind (2) is not explicitly called, it will be implicity invoked by the operating
+//system when one is about to establish a connection and/or send a datagram. An appropriate
+//address will be selected that the remote peer can reach too.
+		//memset(&src_addr, 0, sizeof(src_addr));
+		//src_addr.nl_family = AF_NETLINK;
+		//src_addr.nl_pid = getpid(); /* self pid */
 
-		memset(&src_addr, 0, sizeof(src_addr));
-		src_addr.nl_family = AF_NETLINK;
-		src_addr.nl_pid = getpid(); /* self pid */
-
-		bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
+	//	bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
 
 		memset(&dest_addr, 0, sizeof(dest_addr));
 		memset(&dest_addr, 0, sizeof(dest_addr));
@@ -49,6 +59,7 @@ void main()
 
 		/* Read message from kernel */
 		recvmsg(sock_fd, &msg, 0);
-		printf("Received message payload: %s\n", NLMSG_DATA(nlh));
+		printf("Received message payload: %s\n", (char*)NLMSG_DATA(nlh));
 		close(sock_fd);
+		return 0;
 }
